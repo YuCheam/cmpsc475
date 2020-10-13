0bda18a4b68bebef9d4993fa8b689dc6435e978a
Kha-Yu Cheam <kac6294@psu.edu>
2020-10-13 01:33:14 -0400

    Need to solve binding bug

--- Campus/Campus/Views/BuildingView/DirectionSheet.swift
+++ Campus/Campus/Views/BuildingView/DirectionSheet.swift
@@ -9,34 +9,34 @@ import SwiftUI
 
 struct DirectionSheet: View {
     @EnvironmentObject var locationsManager: LocationsManager
-    @State var startIndex: Int?
-    @State var destinationIndex: Int?
+    @State var startIndex: Int = -1
+    @State var destinationIndex: Int = -1
     @Binding var isShowingSheet: Bool
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Picker(selection: $startIndex, label: Text("From")){
-                        Text("Current Location").tag(nil as Int?)
-                        ForEach(locationsManager.allBuildings.indices){ index in
-                            Text("\(locationsManager.allBuildings[index].name)").tag(index as Int?)
+                    Picker(selection: self.$startIndex, label: Text("From")){
+                        Text("Current Location").tag(-1)
+                        ForEach(locationsManager.allBuildings.indices, id: \.self){ index in
+                            Text("\(locationsManager.allBuildings[index].name)").tag(index)
                         }
-                    }
+                    }.id(UUID())
                     
-                    Picker(selection: $destinationIndex, label: Text("To")){
-                        Text("Current Location").tag(nil as Int?)
-                        ForEach(locationsManager.allBuildings.indices){ index in
-                            Text("\(locationsManager.allBuildings[index].name)").tag(index as Int?)
+                    Picker(selection: self.$destinationIndex, label: Text("To")){
+                        Text("Current Location").tag(-1)
+                        ForEach(locationsManager.allBuildings.indices, id: \.self){ index in
+                            Text("\(locationsManager.allBuildings[index].name)").tag(index)
                         }
-                    }
+                    }.id(UUID())
                 }
                 
                 Section {
                     Button("Get Directions", action: {
-                        let source = startIndex == nil ? nil as Building? : locationsManager.allBuildings[startIndex!]
-                        let destination = destinationIndex == nil ? nil as Building? : locationsManager.allBuildings[destinationIndex!]
-                        
+                        let source = self.startIndex == -1 ? nil as Building? : locationsManager.allBuildings[startIndex]
+                        let destination = self.destinationIndex == -1 ? nil as Building? :
+                            locationsManager.allBuildings[destinationIndex]
                         let route = getDirections(from: source, to: destination)
                     })
                     Button("Dismiss", action: {isShowingSheet.toggle()})
