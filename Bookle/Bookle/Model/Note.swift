83cd0553c7a09834ff015dd337ec251479557220
Kha-Yu Cheam <kac6294@psu.edu>
2020-10-23 17:50:07 -0400

    Created Note Struct

---
+++ Bookle/Bookle/Model/Note.swift
@@ -0,0 +1,30 @@
+//
+//  Note.swift
+//  Bookle
+//
+//  Created by Kha-Yu Cheam on 10/23/20.
+//
+
+import Foundation
+
+struct Note: Identifiable, Hashable {
+    let id: Date
+    let timeOfCreation: Date
+    let pageProgress: Float
+    var noteText: String
+    
+    var formattedDate: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: timeOfCreation)
+    }
+    
+    
+    init(page: Float, text: String) {
+        let _date = Date()
+        timeOfCreation = _date
+        pageProgress = page
+        noteText = text
+        id = _date
+    }
+}
