7f6c7106249ed03774f180a242e6d72222dccff9
Kha-Yu Cheam <kac6294@psu.edu>
2020-11-12 11:28:43 -0500

    new files

---
+++ Headway/Headway/HealthView.swift
@@ -0,0 +1,20 @@
+//
+//  HealthView.swift
+//  Headway
+//
+//  Created by Kha-Yu Cheam on 11/9/20.
+//
+
+import SwiftUI
+
+struct HealthView: View {
+    var body: some View {
+        Text("Health View")
+    }
+}
+
+struct HealthView_Previews: PreviewProvider {
+    static var previews: some View {
+        HealthView()
+    }
+}
---
+++ Headway/Headway/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Headway
+//
+//  Created by Kha-Yu Cheam on 11/9/20.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
